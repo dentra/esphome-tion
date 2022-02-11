5c43fa3afe4e133bfa95ca79d806d23071137741
dentra <dennis.trachuk@gmail.com>
2022-02-11 19:06:33 +0000

    allow direct write for 3s

--- components/tion_3s/tion_3s.h
+++ components/tion_3s/tion_3s.h
@@ -22,22 +22,18 @@ class Tion3s : public TionComponent, public TionClimate, public TionBleNode, pub
   const esp_bt_uuid_t &get_ble_service() const override;
   const esp_bt_uuid_t &get_ble_char_tx() const override;
   const esp_bt_uuid_t &get_ble_char_rx() const override;
-  esp_ble_sec_act_t get_ble_encryption() const override { return ble_sec_enc_; }
-  bool ble_reg_for_notify() const override { return pair_state_ > 0; }
+  esp_ble_sec_act_t get_ble_encryption() const override { return this->ble_sec_enc_; }
+  bool ble_reg_for_notify() const override { return this->pair_state_ > 0; }
 
   void set_ble_encryption(esp_ble_sec_act_t ble_sec_enc) { this->ble_sec_enc_ = ble_sec_enc; }
+  void set_direct_write(bool direct_write) { this->direct_write_ = direct_write; }
 
   void read_data(const uint8_t *data, uint16_t size) override { TionsApi3s::read_data(data, size); }
   bool write_data(const uint8_t *data, uint16_t size) const override { return TionBleNode::write_data(data, size); }
 
   void on_ready() override;
   void read(const tion3s_state_t &state) override;
-  bool write_state() override {
-    this->publish_state();
-    this->dirty_ = true;
-    this->parent_->set_enabled(true);
-    return true;
-  };
+  bool write_state() override;
 
   void pair() {
     this->pair_state_ = -1;
@@ -55,6 +51,7 @@ class Tion3s : public TionComponent, public TionClimate, public TionBleNode, pub
   int8_t pair_state_{};  // 0 - not paired, 1 - paired, -1 - pairing
   void update_state_(tion3s_state_t &state);
   esp_ble_sec_act_t ble_sec_enc_{esp_ble_sec_act_t::ESP_BLE_SEC_ENCRYPT_MITM};
+  bool direct_write_{};
 };
 
 class Tion3sBuzzerSwitch : public switch_::Switch {
