03592a6455c189d6ecdc983e637bf4226e93dbbc
dentra <dennis.trachuk@gmail.com>
2022-11-03 11:39:53 +0000

    vport: fix compilation warning and update logging

--- components/vport/vport_ble.cpp
+++ components/vport/vport_ble.cpp
@@ -8,17 +8,6 @@ namespace vport {
 
 static const char *const TAG = "vport_ble";
 
-static inline bool check_conn_id(ble_client::BLEClient *ble_client, esp_gattc_cb_event_t event, uint16_t conn_id) {
-#ifdef ESPHOME_LOG_HAS_VERBOSE
-  if (conn_id != ble_client->get_conn_id()) {
-    ESP_LOGD(TAG, "[%s][%u] Unexpected param conn_id=%u, open conn_id=%u", ble_client->address_str().c_str(), event,
-             conn_id, ble_client->get_conn_id());
-    return false;
-  }
-  return true;
-#endif
-}
-
 void VPortBLENode::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
                                        esp_ble_gattc_cb_param_t *param) {
   auto ble_client = this->parent_;
@@ -26,19 +15,15 @@ void VPortBLENode::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
 #ifdef ESPHOME_LOG_HAS_VERBOSE
   // This should not be happen, but with bluetooth_proxy ESPHome broke it.
   // So do additional checks in verbose mode.
-  // Also check_conn_id for it.
   if (gattc_if != ble_client->get_gattc_if()) {
-    ESP_LOGD(TAG, "[%s][%u] Unexpected param gattc_if=%u, open gattc_if=%u", ble_client->address_str().c_str(), event,
+    ESP_LOGV(TAG, "[%s][%u] Unexpected param gattc_if=%u, open gattc_if=%u", ble_client->address_str().c_str(), event,
              gattc_if, ble_client->get_gattc_if());
     return;
   }
 #endif
 
   if (event == ESP_GATTC_NOTIFY_EVT) {
-    if (!check_conn_id(ble_client, event, param->notify.conn_id)) {
-      return;
-    }
-    ESP_LOGV(TAG, "Got notify for handle %04u: %s", param->notify.handle,
+    ESP_LOGV(TAG, "[%s] Got notify for handle %04u: %s", ble_client->address_str().c_str(), param->notify.handle,
              format_hex_pretty(param->notify.value, param->notify.value_len).c_str());
     if (param->notify.handle == this->char_rx_) {
       this->on_ble_data(param->notify.value, param->notify.value_len);
@@ -47,42 +32,37 @@ void VPortBLENode::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
   }
 
   if (event == ESP_GATTC_WRITE_DESCR_EVT) {
-    if (!check_conn_id(ble_client, event, param->write.conn_id)) {
-      return;
-    }
-    ESP_LOGV(TAG, "write_char_descr at 0x%x complete 0x%02x", param->write.handle, param->write.status);
+    ESP_LOGV(TAG, "[%s] Complete write_char_descr to 0x%x, status=0x%02x", ble_client->address_str().c_str(),
+             param->write.handle, param->write.status);
     this->node_state = esp32_ble_tracker::ClientState::ESTABLISHED;
     this->on_ble_ready_();
     return;
   }
 
   if (event == ESP_GATTC_SEARCH_CMPL_EVT) {
-    if (!check_conn_id(ble_client, event, param->search_cmpl.conn_id)) {
-      return;
-    }
-
     auto tx = ble_client->get_characteristic(this->ble_service_, this->ble_char_tx_);
     if (tx == nullptr) {
-      ESP_LOGE(TAG, "Can't discover TX characteristics");
+      ESP_LOGE(TAG, "[%s] Can't discover TX characteristics", ble_client->address_str().c_str());
       return;
     }
     this->char_tx_ = tx->handle;
 
     auto rx = ble_client->get_characteristic(this->ble_service_, this->ble_char_rx_);
     if (rx == nullptr) {
-      ESP_LOGE(TAG, "Can't discover RX characteristics");
+      ESP_LOGE(TAG, "[%s] Can't discover RX characteristics", ble_client->address_str().c_str());
       return;
     }
     this->char_rx_ = rx->handle;
 
-    ESP_LOGD(TAG, "Discovering complete");
-    ESP_LOGV(TAG, "  TX handle 0x%x", this->char_tx_);
-    ESP_LOGV(TAG, "  RX handle 0x%x", this->char_rx_);
+    ESP_LOGD(TAG, "[%s] Discovering complete", ble_client->address_str().c_str());
+    ESP_LOGV(TAG, "[%s]  TX handle 0x%x", ble_client->address_str().c_str(), this->char_tx_);
+    ESP_LOGV(TAG, "[%s]  RX handle 0x%x", ble_client->address_str().c_str(), this->char_rx_);
 
     if (this->ble_reg_for_notify()) {
       auto err =
           esp_ble_gattc_register_for_notify(ble_client->get_gattc_if(), ble_client->get_remote_bda(), this->char_rx_);
-      ESP_LOGV(TAG, "Register for notify 0x%x complete: %s", this->char_rx_, YESNO(err == ESP_OK));
+      ESP_LOGV(TAG, "[%s] Register for notify 0x%x complete: %s", ble_client->address_str().c_str(), this->char_rx_,
+               YESNO(err == ESP_OK));
     } else {
       this->node_state = esp32_ble_tracker::ClientState::ESTABLISHED;
       this->on_ble_ready_();
@@ -92,16 +72,12 @@ void VPortBLENode::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
   }
 
   if (event == ESP_GATTC_CONNECT_EVT) {
-    if (!check_conn_id(ble_client, event, param->connect.conn_id)) {
-      return;
-    }
-
     // let the device to do pair
     if (this->ble_sec_act_ != 0) {
       auto err = esp_ble_set_encryption(param->connect.remote_bda, this->ble_sec_act_);
-      ESP_LOGV(TAG, "Bonding complete: %s", YESNO(err == ESP_OK));
+      ESP_LOGV(TAG, "[%s] Bonding complete: %s", ble_client->address_str().c_str(), YESNO(err == ESP_OK));
     } else {
-      ESP_LOGV(TAG, "Bonding skipped");
+      ESP_LOGV(TAG, "[%s] Bonding skipped", ble_client->address_str().c_str());
     }
     if (this->disable_scan_) {
       esp32_ble_tracker::global_esp32_ble_tracker->stop_scan();
@@ -110,10 +86,6 @@ void VPortBLENode::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
   }
 
   if (event == ESP_GATTC_DISCONNECT_EVT) {
-    if (!check_conn_id(ble_client, event, param->disconnect.conn_id)) {
-      return;
-    }
-
     if (this->disable_scan_) {
       esp32_ble_tracker::global_esp32_ble_tracker->start_scan();
     }
@@ -121,21 +93,18 @@ void VPortBLENode::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
 
 #ifdef ESPHOME_LOG_HAS_VERBOSE
   if (event == ESP_GATTC_REG_FOR_NOTIFY_EVT) {
-    ESP_LOGV(TAG, "Registring for notify complete");
+    ESP_LOGV(TAG, "[%s] Registring for notify complete", ble_client->address_str().c_str());
     return;
   }
 
   if (event == ESP_GATTC_WRITE_CHAR_EVT) {
-    if (!check_conn_id(ble_client, event, param->write.conn_id)) {
-      return;
-    }
-    ESP_LOGV(TAG, "Complete write_char to 0x%x, status=0x%02x", param->write.conn_id, param->write.handle,
-             param->write.status);
+    ESP_LOGV(TAG, "[%s] Complete write_char to 0x%x, status=0x%02x", ble_client->address_str().c_str(),
+             param->write.conn_id, param->write.handle, param->write.status);
     return;
   }
 
   if (event == ESP_GATTC_ENC_CMPL_CB_EVT) {
-    ESP_LOGV(TAG, "Complete esp_ble_set_encryption");
+    ESP_LOGV(TAG, "[%s] Complete esp_ble_set_encryption", ble_client->address_str().c_str());
     return;
   }
 #endif
@@ -169,19 +138,21 @@ bool VPortBLENode::write_ble_data(const uint8_t *data, uint16_t size) {
   this->connect();
   return true;
 #else
-  ESP_LOGW(TAG, "Not connected, can't write: %s", format_hex_pretty(data, size).c_str());
+  ESP_LOGW(TAG, "[%s] Not connected, can't write: %s", this->parent_->address_str().c_str(),
+           format_hex_pretty(data, size).c_str());
   return false;
 #endif
 }
 
 bool VPortBLENode::write_ble_data_(const uint8_t *data, uint16_t size) const {
-  ESP_LOGV(TAG, "Writing data to 0x%x: %s", this->char_tx_, format_hex_pretty(data, size).c_str());
+  auto ble_client = this->parent_;
+  ESP_LOGV(TAG, "[%s] Writing data to 0x%x: %s", ble_client->address_str().c_str(), this->char_tx_,
+           format_hex_pretty(data, size).c_str());
 #ifdef ESPHOME_LOG_HAS_VERBOSE
   auto write_type = ESP_GATT_WRITE_TYPE_RSP;
 #else
   auto write_type = ESP_GATT_WRITE_TYPE_NO_RSP;
 #endif
-  auto ble_client = this->parent_;
   return esp_ble_gattc_write_char(ble_client->get_gattc_if(), ble_client->get_conn_id(), this->char_tx_, size,
                                   const_cast<uint8_t *>(data), write_type, ESP_GATT_AUTH_REQ_NONE) == ESP_OK;
 }
@@ -189,7 +160,7 @@ bool VPortBLENode::write_ble_data_(const uint8_t *data, uint16_t size) const {
 void VPortBLENode::schedule_disconnect(Component *component, uint32_t timeout) {
   if (timeout) {
     App.scheduler.set_timeout(component, TAG, timeout, [this]() {
-      ESP_LOGV(TAG, "Disconnecting");
+      ESP_LOGV(TAG, "[%s] Disconnecting", this->parent()->address_str().c_str());
       this->disconnect();
     });
   }
