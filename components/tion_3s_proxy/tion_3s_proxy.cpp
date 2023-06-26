ef9cb9091df7a240a5a67ebb8c9a7b66fb2dba1f
dentra <dennis.trachuk@gmail.com>
2023-06-26 18:05:01 +0000

    3s_proxy: forward only requested commands responses

--- components/tion_3s_proxy/tion_3s_proxy.cpp
+++ components/tion_3s_proxy/tion_3s_proxy.cpp
@@ -7,16 +7,24 @@ namespace tion_3s_proxy {
 
 static const char *const TAG = "tion_3s_proxy";
 
+#define FRAME_REQ_TO_CMD(req) (req >> 8)
+#define FRAME_RSP_TO_CMD(rsp) (rsp >> 12)
+
 void TionApi3sProxy::read_frame(uint16_t frame_type, const void *frame_data, size_t frame_data_size) {
+  if (this->parent_->last_cmd_ != FRAME_RSP_TO_CMD(frame_type)) {
+    return;
+  }
   auto data8 = static_cast<const uint8_t *>(frame_data);
   ESP_LOGD(TAG, "RX (%04X): %s", frame_type, format_hex_pretty(data8, frame_data_size).c_str());
-  this->tx_->write_frame(frame_type, frame_data, frame_data_size);
+  this->parent_->tx_->write_frame(frame_type, frame_data, frame_data_size);
+  this->parent_->last_cmd_ = 0;
 }
 
 void Tion3sProxy::on_frame_(const dentra::tion::TionUartProtocol3s::frame_spec_type &frame, size_t size) {
   auto frame_data_size = size - frame.head_size();
   ESP_LOGD(TAG, "TX (%04X): %s", frame.type, format_hex_pretty(frame.data, frame_data_size).c_str());
   this->rx_->write_frame(frame.type, frame.data, frame_data_size);
+  this->last_cmd_ = FRAME_REQ_TO_CMD(frame.type);
 }
 
 void Tion3sProxy::dump_config() { ESP_LOGCONFIG(TAG, "Tion 3S Proxy"); }
--- components/tion_3s_proxy/tion_3s_proxy.h
+++ components/tion_3s_proxy/tion_3s_proxy.h
@@ -25,21 +25,25 @@ class Tion3sUartIO : public tion::TionUartIO<TionUartProtocol3sProxy> {
   }
 };
 
+class Tion3sProxy;
+
 class TionApi3sProxy : public dentra::tion::TionApiBase<dentra::tion::tion3s_state_t> {
  public:
   void read_frame(uint16_t frame_type, const void *frame_data, size_t frame_data_size);
-  void set_tx(Tion3sUartIO *tx) { this->tx_ = tx; }
+  void set_tx(Tion3sProxy *parent) { this->parent_ = parent; }
 
  protected:
-  Tion3sUartIO *tx_{};
+  Tion3sProxy *parent_{};
 };
 
 class Tion3sProxy : public Component {
+  friend class TionApi3sProxy;
+
  public:
   explicit Tion3sProxy(TionApi3sProxy *rx, uart::UARTComponent *uart) : rx_(rx) {
     this->tx_ = new Tion3sUartIO(uart);
     this->tx_->set_on_frame(Tion3sUartIO::on_frame_type::create<Tion3sProxy, &Tion3sProxy::on_frame_>(*this));
-    this->rx_->set_tx(this->tx_);
+    this->rx_->set_tx(this);
   }
   virtual ~Tion3sProxy() { delete this->tx_; }
   void dump_config() override;
@@ -51,6 +55,8 @@ class Tion3sProxy : public Component {
   TionApi3sProxy *rx_{};
   // ble module
   Tion3sUartIO *tx_;
+
+  uint8_t last_cmd_{};
 };
 
 }  // namespace tion_3s_proxy
