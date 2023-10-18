f1284df94efebbafaf774b0e982b3bccc3c0df50
dentra <dennis.trachuk@gmail.com>
2023-10-18 19:56:55 +0300

    cleanup 3s python configuration

--- components/tion_3s/__init__.py
+++ components/tion_3s/__init__.py
@@ -1,11 +1,9 @@
+import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.cpp_generator import MockObjClass
-from esphome.components import select, sensor
+from esphome.components import select
 from esphome.const import (
     ENTITY_CATEGORY_CONFIG,
-    ENTITY_CATEGORY_DIAGNOSTIC,
-    STATE_CLASS_MEASUREMENT,
-    UNIT_CUBIC_METER,
 )
 from .. import tion  # pylint: disable=relative-beyond-top-level
 
@@ -13,6 +11,7 @@ CODEOWNERS = ["@dentra"]
 AUTO_LOAD = ["tion", "select"]
 
 CONF_AIR_INTAKE = "air_intake"
+CONF_TURN_OFF_BEFORE_HEAT = "turn_off_before_heat"
 
 Tion3sApi = tion.tion_ns.class_("Tion3sApi")
 Tion3sAirIntakeSelectT = tion.tion_ns.class_("Tion3sAirIntakeSelect", select.Select)
@@ -28,6 +27,7 @@ def tion_3s_schema(tion_class: MockObjClass, tion_base_schema: cv.Schema):
                 icon="mdi:air-conditioner",
                 entity_category=ENTITY_CATEGORY_CONFIG,
             ),
+            cv.Optional(CONF_TURN_OFF_BEFORE_HEAT, default=True): cv.boolean,
         }
     )
 
@@ -38,3 +38,5 @@ async def setup_tion_3s(config, compoent_reg):
     await tion.setup_select(
         config, CONF_AIR_INTAKE, var.set_air_intake, var, OPTIONS_AIR_INTAKE
     )
+    if config[CONF_TURN_OFF_BEFORE_HEAT]:
+        cg.add_define("USE_TION_ENABLE_OFF_BEFORE_HEAT")
