4c9d6d6b92626490f1f4d5104179d88041737e84
dentra <dennis.trachuk@gmail.com>
2024-07-08 18:21:52 +0300

    add str_sprintf to api

--- components/tion-api/utils.h
+++ components/tion-api/utils.h
@@ -19,12 +19,14 @@ inline std::string tion_hexencode(const void *data, uint32_t size) {
 inline void yield() { esphome::yield(); }
 inline uint32_t millis() { return esphome::millis(); }
 using esphome::optional;
+using esphome::str_sprintf;
 #else
 #define PACKED __attribute__((packed))
 std::string tion_hexencode(const void *data, uint32_t size);
 inline void yield() {}
 #include <optional>
 using std::optional;
+std::string __attribute__((format(printf, 1, 2))) str_sprintf(const char *fmt, ...);
 #endif
 
 #if !__has_builtin(__builtin_bswap16)
