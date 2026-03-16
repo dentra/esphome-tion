63a93ba8cd0e324b97557f2c72724e21da591ad1
dentra <dennis.trachuk@gmail.com>
2026-03-16 20:37:20 +0300

    rearrange substitutions

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,25 +1,59 @@
 #!/bin/bash
 
+# example usage: run-tests.sh -t lt -p uart -c api -v
+
 prj_path=$(dirname $0)/..
 
 types=(3s 4s lt o2)
 ports=(ble uart)
 conns=(api mqtt)
 
+custom=false
+
+while getopts ":v:t:p:c:" opt; do
+  case $opt in
+    t)
+      types=($OPTARG)
+      custom=true
+      ;;
+    p)
+      ports=($OPTARG)
+      custom=true
+      ;;
+    c)
+      conns=($OPTARG)
+      custom=true
+      ;;
+    v)
+      verbose=-v
+      echo "Enabling verbose mode"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+shift $((OPTIND-1)) # Shift positional parameters past the options
+
 test_dir=$prj_path/tests/gen
 conf_dir=$prj_path/configs
 
 ESPHOME_DATA_DIR=$(realpath $prj_path/.nosync/esphome-tests)
+
+echo "Configuration types: ${types[@]}"
+echo "Configuration ports: ${ports[@]}"
+echo "Configuration conns: ${conns[@]}"
 
 for type in "${types[@]}"; do
   for port in "${ports[@]}"; do
     for conn in "${conns[@]}"; do
       conf=$test_dir/tion-$type-$port-$conn.yaml
       if [ -f "$conf" ]; then
-        esphome compile $conf || exit
+        esphome $verbose compile $conf || exit
       fi
     done
   done
 done
 
-esphome compile $test_dir/tion-multiple.yaml
+[ "$custom" != "true" ] && esphome compile $verbose $test_dir/tion-multiple.yaml
